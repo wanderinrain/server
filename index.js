e89179e8556522394ccca69488a721e6e8917fc6
wanderinrain <47468659+wanderinrain@users.noreply.github.com>
2020-02-14 19:21:39 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require('express');
+
+const mongoose = require('mongoose');
+
+const cookieSession = require('cookie-session');
+
+const passport = require('passport');
+
+const keys = require('./config/keys');
+
+
+require('./models/User');
+
+require('./services/passport');
+
+
+
+mongoose.connect(keys.mongoURI);
+
+
+
+const app = express();
+
+
+app.use(
+
+    cookieSession({
+        
+        //how long it can exist in browser: 30days
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        
+        
+        keys: [keys.cookieKey]
+
+        
+        
+    })
+
+);
+
+app.use(passport.initialize());
+
+
+app.use(passport.session());
+
+
+require('./routes/authRoutes')(app);
+
+
+
+const PORT = process.env.PORT || 5000;
+
+
+app.listen(PORT);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
